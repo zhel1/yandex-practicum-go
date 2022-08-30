c54c6bf6a2b79ff25d480dd3dcee2617dffcd1da
Denis Zheleznov <zhel1@yandex.ru>
2022-08-30 21:30:45 +0300

    Sprint 9. Increment 20, 21.

---
+++ internal/certificates/certificates.go
@@ -0,0 +1,111 @@
+//Package certificates provides functions to create self signed certificates
+package certificates
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"math/big"
+	"net"
+	"time"
+)
+
+func genCert(template, parent *x509.Certificate, publicKey *rsa.PublicKey, privateKey *rsa.PrivateKey) (*x509.Certificate, []byte) {
+	certBytes, err := x509.CreateCertificate(rand.Reader, template, parent, publicKey, privateKey)
+	if err != nil {
+		panic("Failed to create certificate:" + err.Error())
+	}
+
+	cert, err := x509.ParseCertificate(certBytes)
+	if err != nil {
+		panic("Failed to parse certificate:" + err.Error())
+	}
+
+	b := pem.Block{Type: "CERTIFICATE", Bytes: certBytes}
+	certPEM := pem.EncodeToMemory(&b)
+
+	return cert, certPEM
+}
+
+func GenCARoot() (*x509.Certificate, []byte, *rsa.PrivateKey) {
+	var rootTemplate = x509.Certificate{
+		SerialNumber: big.NewInt(1),
+		Subject: pkix.Name{
+			Country:      []string{"SE"},
+			Organization: []string{"Company Co."},
+			CommonName:   "Root CA",
+		},
+		NotBefore:             time.Now().Add(-10 * time.Second),
+		NotAfter:              time.Now().AddDate(10, 0, 0),
+		KeyUsage:              x509.KeyUsageCertSign | x509.KeyUsageCRLSign,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+		IsCA:                  true,
+		MaxPathLen:            2,
+		IPAddresses:           []net.IP{net.ParseIP("127.0.0.1")},
+	}
+	priv, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		panic(err)
+	}
+	rootCert, rootPEM := genCert(&rootTemplate, &rootTemplate, &priv.PublicKey, priv)
+	return rootCert, rootPEM, priv
+}
+
+func GenDCA(RootCert *x509.Certificate, RootKey *rsa.PrivateKey) (*x509.Certificate, []byte, *rsa.PrivateKey) {
+	priv, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		panic(err)
+	}
+
+	var DCATemplate = x509.Certificate{
+		SerialNumber: big.NewInt(1),
+		Subject: pkix.Name{
+			Country:      []string{"SE"},
+			Organization: []string{"Company Co."},
+			CommonName:   "DCA",
+		},
+		NotBefore:             time.Now().Add(-10 * time.Second),
+		NotAfter:              time.Now().AddDate(10, 0, 0),
+		KeyUsage:              x509.KeyUsageCertSign | x509.KeyUsageCRLSign,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+		IsCA:                  true,
+		MaxPathLenZero:        false,
+		MaxPathLen:            1,
+		IPAddresses:           []net.IP{net.ParseIP("127.0.0.1")},
+	}
+	DCACert, DCAPEM := genCert(&DCATemplate, RootCert, &priv.PublicKey, RootKey)
+	return DCACert, DCAPEM, priv
+}
+
+func GenServerCert(DCACert *x509.Certificate, DCAKey *rsa.PrivateKey) (*x509.Certificate, []byte, *rsa.PrivateKey) {
+	priv, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		panic(err)
+	}
+
+	var ServerTemplate = x509.Certificate{
+		SerialNumber: big.NewInt(1),
+		NotBefore:    time.Now().Add(-10 * time.Second),
+		NotAfter:     time.Now().AddDate(10, 0, 0),
+		KeyUsage:     x509.KeyUsageCertSign | x509.KeyUsageDataEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage: []x509.ExtKeyUsage{
+			x509.ExtKeyUsageAny,
+			x509.ExtKeyUsageServerAuth,
+			x509.ExtKeyUsageClientAuth,
+			x509.ExtKeyUsageCodeSigning,
+			x509.ExtKeyUsageEmailProtection,
+			x509.ExtKeyUsageTimeStamping,
+			x509.ExtKeyUsageOCSPSigning,
+		},
+		IsCA:           false,
+		MaxPathLenZero: true,
+		IPAddresses:    []net.IP{net.ParseIP("127.0.0.1")},
+	}
+
+	ServerCert, ServerPEM := genCert(&ServerTemplate, DCACert, &priv.PublicKey, DCAKey)
+	return ServerCert, ServerPEM, priv
+}
--- internal/config/config.go
+++ internal/config/config.go
@@ -2,7 +2,10 @@
 package config
 
 import (
+	"encoding/json"
 	"flag"
+	"fmt"
+	"os"
 	"strings"
 
 	"github.com/caarlos0/env/v6"
@@ -10,30 +13,103 @@ import (
 
 //Config contains variables to configure app
 type Config struct {
-	Addr            string `env:"SERVER_ADDRESS"`
-	BaseURL         string `env:"BASE_URL"`
-	FileStoragePath string `env:"FILE_STORAGE_PATH"`
-	UserKey         string `env:"USER_KEY" envDefault:"PaSsW0rD"`
-	DatabaseDSN     string `env:"DATABASE_DSN"`
+	Addr            string `env:"SERVER_ADDRESS"     json:"server_address"`
+	BaseURL         string `env:"BASE_URL"           json:"base_url"`
+	FileStoragePath string `env:"FILE_STORAGE_PATH"  json:"file_storage_path"`
+	UserKey         string `env:"USER_KEY" envDefault:"PaSsW0rD" json:"user_key"`
+	DatabaseDSN     string `env:"DATABASE_DSN"       json:"database_dsn"`
+	EnableHTTPS     bool   `env:"ENABLE_HTTPS"       json:"enable_https"`
+	Config          string `env:"CONFIG"             json:"-"`
+}
+
+func (c Config) String() string {
+	return fmt.Sprintf(
+		"Configuration:\n"+
+			"  Addr: %s\n"+
+			"  BaseURL: %s\n"+
+			"  FileStoragePath: %s\n"+
+			"  UserKey: %s\n"+
+			"  DatabaseDSN: %s\n"+
+			"  EnableHTTPS: %t\n", c.Addr, c.BaseURL, c.FileStoragePath, c.UserKey, c.DatabaseDSN, c.EnableHTTPS,
+	)
 }
 
 //DatabaseDSN scheme: "postgres://username:password@localhost:5432/database_name?sslmode=disable"
 
 //Parse parses command line parameters or sets defaults (if parameters were not set) and then overrides them by environment variables
 func (c *Config) Parse() error {
-	flag.StringVar(&c.Addr, "a", "localhost:8080", "Host to listen on")
-	flag.StringVar(&c.BaseURL, "b", "http://localhost:8080/", "Base address of the resulting shortened URL")
-	flag.StringVar(&c.FileStoragePath, "f", "", "Path to the file with shortened URLs")
-	flag.StringVar(&c.UserKey, "p", "", "UserKey for encryption cookie")
-	flag.StringVar(&c.DatabaseDSN, "d", "", "The line with the address to connect to the database")
+	// priority: config file -> flags -> env
+
+	tempConf := Config{}
+	flag.StringVar(&tempConf.Addr, "a", "localhost:8080", "Host to listen on")
+	flag.StringVar(&tempConf.BaseURL, "b", "localhost:8080/", "Base address of the resulting shortened URL")
+	flag.StringVar(&tempConf.FileStoragePath, "f", "", "Path to the file with shortened URLs")
+	flag.StringVar(&tempConf.UserKey, "p", "", "UserKey for encryption cookie")
+	flag.StringVar(&tempConf.DatabaseDSN, "d", "", "The line with the address to connect to the database")
+	flag.BoolVar(&tempConf.EnableHTTPS, "s", false, "Enable HTTPS mode in web-server")
+	flag.StringVar(&tempConf.Config, "config", "", "Config file")
+	flag.StringVar(&tempConf.Config, "c", "", "Config file")
 	flag.Parse()
 
-	//settings redefinition, if evn variables is used
+	if isFlagPassed("config") || isFlagPassed("c") {
+		data, err := os.ReadFile(tempConf.Config)
+		if err != nil {
+			return err
+		}
+
+		err = json.Unmarshal(data, c)
+		if err != nil {
+			return err
+		}
+	}
+
+	// settings redefinition from flags
+	if isFlagPassed("a") || c.Addr == "" {
+		c.Addr = tempConf.Addr
+	}
+	if isFlagPassed("b") || c.BaseURL == "" {
+		c.BaseURL = tempConf.BaseURL
+	}
+	if isFlagPassed("f") || c.FileStoragePath == "" {
+		c.FileStoragePath = tempConf.FileStoragePath
+	}
+	if isFlagPassed("p") || c.UserKey == "" {
+		c.UserKey = tempConf.UserKey
+	}
+	if isFlagPassed("d") || c.DatabaseDSN == "" {
+		c.DatabaseDSN = tempConf.DatabaseDSN
+	}
+	if isFlagPassed("s") {
+		c.EnableHTTPS = tempConf.EnableHTTPS
+	}
+
+	// settings redefinition from evn
 	err := env.Parse(c)
+
+	if !strings.HasPrefix(c.BaseURL, "http") {
+		if c.EnableHTTPS {
+			c.BaseURL = "https://" + c.BaseURL
+		} else {
+			c.BaseURL = "http://" + c.BaseURL
+		}
+	}
 
 	if !strings.HasSuffix(c.BaseURL, "/") {
 		c.BaseURL = c.BaseURL + "/"
 	}
 
+	fmt.Println(c)
+
 	return err
+}
+
+// isFlagPassed checks whether the flag was set in CLI
+func isFlagPassed(name string) bool {
+	found := false
+	flag.Visit(func(f *flag.Flag) {
+		if f.Name == name {
+			found = true
+		}
+	})
+	return found
 }
